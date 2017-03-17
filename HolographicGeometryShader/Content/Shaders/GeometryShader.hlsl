9120b2742cc0f49bc50b600159eec8c852e527f8
Jaewon Choi <jainersoer@ajou.ac.kr>
2017-03-17 21:16:58 +0900

    Run the circle

--- HolographicGeometryShader/Content/Shaders/GeometryShader.hlsl
+++ HolographicGeometryShader/Content/Shaders/GeometryShader.hlsl
@@ -14,9 +14,11 @@ struct GeometryShaderOutput
     uint        rtvId   : SV_RenderTargetArrayIndex;
 };
 
+#define CIRCLE_PRECISION 4
+
 // This geometry shader is a pass-through that leaves the geometry unmodified 
 // and sets the render target array index.
-[maxvertexcount(3)]
+[maxvertexcount(CIRCLE_PRECISION * 3)]
 void main(point GeometryShaderInput input[1], inout TriangleStream<GeometryShaderOutput> outStream)
 {
     /*
@@ -46,25 +48,26 @@ void main(point GeometryShaderInput input[1], inout TriangleStream<GeometryShade
     min16float4 center = input[0].pos;
 
     float PI = 3.141592;
+    float radius = 0.3f;
 
-    [unroll(3)]
-    for ( int i = 0; i < 3; ++i )
+    [unroll(CIRCLE_PRECISION)]
+    for ( int i = 0; i < CIRCLE_PRECISION; ++i )
     {
-        float t1 = (float) i / 3;
+        float t1 = (float) i / CIRCLE_PRECISION;
         float cost1 = cos(t1 * PI * 2);
         float sint1 = sin(t1 * PI * 2);
         
-        float t2 = (float) (i + 1) / 3;
+        float t2 = (float) (i + 1) / CIRCLE_PRECISION;
         float cost2 = cos(t2 * PI * 2);
         float sint2 = sin(t2 * PI * 2);
 
         output.pos = center;
         outStream.Append(output);
 
-        output.pos = input[0].pos + 0.3f * min16float4(cost2, sint2, 0, 0);
+        output.pos = center + radius * min16float4(cost1, sint1, 0, 0);
         outStream.Append(output);
 
-        output.pos = input[0].pos + 0.3f * min16float4(cost1, sint1, 0, 0);
+        output.pos = center + radius * min16float4(cost2, sint2, 0, 0);
         outStream.Append(output);
     }
 }
